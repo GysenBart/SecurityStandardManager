a43f869eb8c088da34dbcb3f54afd632b1bfbf0f
Gysen Bart <bart-gysen@hotmail.com>
2025-01-11 10:37:56 +0100

    created weekly task

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from app import app
+from app.util import run_cloning_repo
 from config import debug_enabled, host, port
+import threading
 
 if __name__ == "__main__":
+    thread = threading.Thread(target=run_cloning_repo)
+    thread.start()
     app.run(debug=debug_enabled, host=host, port=port)
