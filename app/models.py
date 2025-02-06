86f8c350b146f2a45540836dbb9c600031616b3a
Gysen Bart <bart-gysen@hotmail.com>
2025-02-06 21:24:35 +0100

    Some changes in the models

--- app/models.py
+++ app/models.py
@@ -19,6 +19,7 @@ class User(db.Model):
 class SecurityDomains(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(100))
+    description = db.Column(db.Text)
     
     def __str__(self):
         return self.name
@@ -26,7 +27,7 @@ class SecurityDomains(db.Model):
 class SecurityControls(db.model):
     id = db.Column(db.String(15), primary_key=True)
     name = db.Column(db.String(50))
-    discription = db.Column(db.Text)
+    description = db.Column(db.Text)
     
     def __str__(self):
         return self.name
@@ -34,7 +35,7 @@ class SecurityControls(db.model):
 class SecurityStandards(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(50))
-    discription = db.Column(db.Text)
+    description = db.Column(db.Text)
     
     def __str__(self):
         return self.name
@@ -42,7 +43,7 @@ class SecurityStandards(db.Model):
 class Clausule(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(50))
-    discription = db.Column(db.Text)
+    description = db.Column(db.Text)
     created_at = db.Column(db.DataTime, default=datetime.utcnow)
     
     def __str__(self):
@@ -50,7 +51,8 @@ class Clausule(db.Model):
     
 class ClausuleComment(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    clausule_id = db.Column(db.Integer, db.ForeignKey('clausule.id'))
+    #dsc_id = db.Column(UUID(as_uuid=True), db.ForeignKey(''))
+    clausule_id = db.Column(UUID(as_uuid=True), db.ForeignKey('clausule.id'))
     comment = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
@@ -60,9 +62,9 @@ class ClausuleComment(db.Model):
 class DomainStandardClausule(db.model):
     # create all the relationships here
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    domain_id = db.Column(db.Integer, db.ForeignKey('security_domain.id'))
-    standard_id = db.Column(db.Integer, db.ForeignKey('security_standard.id'))
-    clausule_id = db.Column(db.Integer, db.ForeignKey('clausule.id'))
+    domain_id = db.Column(UUID(as_uuid=True), db.ForeignKey('security_domain.id'))
+    standard_id = db.Column(UUID(as_uuid=True), db.ForeignKey('security_standard.id'))
+    clausule_id = db.Column(UUID(as_uuid=True), db.ForeignKey('clausule.id'))
     version = db.Column(db.String(10))
     start_date = db.Column(db.DateTime, default=datetime.utcnow)
     end_date = db.Column(db.DateTime)
